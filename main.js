a831e3fa9e07d7821203697a484545f1064b90cb
MaxCLZ <maxence.clusaz@gmail.com>
2023-01-06 16:01:59 +0100

    add burgerMaker and display of ingredients

--- main.js
+++ main.js
@@ -39,6 +39,49 @@ const returnExtraTopping = () => {
 }
 
 
+const burgerMaker = () => {
+
+    const bun = returnRandBun();
+    const topping = returnRandTopping();
+    const cheese = returnRandCheese();
+    console.log(cheese);
+    const extraTopping = returnExtraTopping();
+    const sauce= returnRandSauce();
+    const meat= returnRandMeat()
+    let burger={
+        _bun: bun,
+        _topping: topping,
+        _extraTopping: extraTopping,
+        _meat: meat,
+        _cheese: cheese,
+        _sauce: sauce,
+        get bun() {
+            return `Bun : ${this._bun}\n`;
+        },
+        get topping() {
+            return this._extraTopping ? `Toppings : ${this._topping}\nOh you look hungry ! Here an extra topping :  ${this._extraTopping}\n` :  `Topping : ${this._topping}\n`;
+        },
+        get cheese() {
+            return `Cheese : ${this._cheese}\n`;
+        },
+        get sauce() {
+            return `Bun : ${this._sauce}\n`;
+        },
+        get extraTopping() {
+            return this._extraTopping ? this._extraTopping : `No extra toppings for you\n`;
+        },
+        get meat() {
+            return `Meat : ${this._meat}\n`;
+        },
+        get burgerIngredients() {
+            return this.bun + this.topping + this.sauce + this.meat + this.cheese ;
+        }
+
+    }
+    return burger;
+
+}
+
 
 
 
@@ -49,9 +92,17 @@ const returnExtraTopping = () => {
 // console.log(returnRandMeat())
 // console.log(returnRandCheese())
 // console.log(returnExtraTopping())
+// const burger = burgerMaker()
+// console.log(burger.bun);
+// console.log(burger.chesse);
+// console.log(burger.meat);
+// console.log(burger.sauce);
+// console.log(burger.topping);
+// console.log(burger.extraTopping);
+// console.log(burger.burgerIngredients);
 
 
 
-
-
-
+/* Script (node main.js in terminal to run) */
+const burger = burgerMaker()
+console.log(burger.burgerIngredients);
